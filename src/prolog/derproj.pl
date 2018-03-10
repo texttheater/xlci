1c9a1f5d88ff30279cf7c2af9dd586d34e47a6bb
Kilian Evang <noreply@texttheater.net>
2018-03-10 09:41:01 +0100

    Removed comment about overgeneration
    This is taken care of by sort/2 now, I think.

--- src/prolog/derproj.pl
+++ src/prolog/derproj.pl
@@ -92,8 +92,7 @@ transfer_categories(ForeignSentences, EnglishSentences) :-
         )
       ) ).
 
-% Asserts target_typechanger/3 facts mapping target tokens to typechangers. This
-% overgenerates a bit as we need each typechanger only once per sentence.
+% Asserts target_typechanger/3 facts mapping target tokens to typechangers.
 transfer_typechangers :-
   findall(target_typechanger(SID, ForFrom, ForTo, TC),
       ( source_typechanger(SID, TCFrom, TCTo, TC),
--- src/python/ucto_wrapper.py
+++ src/python/ucto_wrapper.py
@@ -11,7 +11,7 @@ import sys
 
 if __name__ == '__main__':
     for line in sys.stdin.buffer:
-        cp = subprocess.run(('ucto',), input=line, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, check=True)
+        cp = subprocess.run(('ucto', '-c', 'ext/ucto/config/tokconfig-generic'), input=line, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, check=True)
         output = cp.stdout
         num_sentences = output.count(b'<utt>')
         assert num_sentences >= 1
