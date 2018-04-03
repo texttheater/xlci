cd76732d2bd1ff86266189bb20dd5fd4ac82cf03
Kilian Evang <noreply@texttheater.net>
2018-04-03 09:52:45 +0200

    cac_renumber.py now ignores non-derivation blocks
    More robust across diverse flavors of the EasyCCG boxer format.

--- src/python/cac_renumber.py
+++ src/python/cac_renumber.py
@@ -10,5 +10,6 @@ if __name__ == '__main__':
     start_pattern = re.compile(r'^ccg\(\d+,')
     next_number = 1
     for block in utils.blocks(sys.stdin):
-        print start_pattern.sub('ccg({},'.format(next_number), block, 1),
-        next_number += 1
+        if block.startswith('ccg('):
+            print start_pattern.sub('ccg({},'.format(next_number), block, 1),
+            next_number += 1
