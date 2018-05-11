6d631edacc6a1ca366aff8ee3caccc301de411d6
Kilian Evang <noreply@texttheater.net>
2018-05-12 01:01:28 +0200

    More data (full current Tatoeba export)

--- src/python/ucto_wrapper.py
+++ src/python/ucto_wrapper.py
@@ -11,8 +11,8 @@ import sys
 
 if __name__ == '__main__':
     for line in sys.stdin.buffer:
-        #cp = subprocess.run(('ucto', '-c', 'ext/ucto/config/tokconfig-generic'), input=line, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, check=True)
-        cp = subprocess.run(('ucto'), input=line, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, check=True)
+        cp = subprocess.run(('ucto', '-c', 'ext/ucto/config/tokconfig-generic'), input=line, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, check=True)
+        #cp = subprocess.run(('ucto'), input=line, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, check=True)
         output = cp.stdout
         num_sentences = output.count(b'<utt>')
         assert num_sentences >= 1
