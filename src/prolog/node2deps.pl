9ae695fe1cce365682a48a8a4a4239d698f2a19b
Kilian Evang <noreply@texttheater.net>
2018-05-31 14:57:22 +0200

    Dependency conversion styles depending on corpus

--- src/prolog/node2deps.pl
+++ src/prolog/node2deps.pl
@@ -1,6 +1,8 @@
 :- module(node2deps, [
     main/0]).
 
+:- use_module(cat, [
+    strip_features/2]).
 :- use_module(catobj, [
     co2cat/2,
     co_res_arg/3,
@@ -17,11 +19,11 @@
 :- use_module(util, [
     argv/1,
     term_in_file/3,
-    write_clause/3]).
+    write_clause/2]).
 
 main :-
   argv([NodeFile, Style]),
-  assertion(member(Style, [plain, mod, det, alpino])),
+  assertion(member(Style, [plain, mod, det, bisk_nl, bisk_esdept, bisk_sv, ud])),
   forall(
       ( term_in_file(node(_, Node), NodeFile, [module(slashes)])
       ),
@@ -80,7 +82,21 @@ node2deps(Node, Style, Deps) :-
         depdirs(Style, Cat, Dirs),
         co_dirs_deps_tok_target(Style, Node, CO, Dirs, Deps, Tok, Tok, _),
         member(Dep, Deps)
-      ), Deps).
+      ), Deps0),
+      %maplist(write_clause(user_error), Deps0),
+      %format(user_error, '~~~n', []),
+  fix_coord(Style, Deps0, Deps).
+  %maplist(write_clause(user_error), Deps).
+
+% The coordination dependency scheme used for Spanish, German and Portuguese
+% cannot be expressed as a special case of our conversion algorithm, so we
+% convert it to UD-style first and then fix it here.
+fix_coord(bisk_esdept, Deps0, Deps) :-
+  member(dep(X1, X2, X\X), Deps0),
+  select(dep(X2, CC, (X\X)/X), Deps0, Deps1),
+  !,
+  fix_coord(bisk_esdept, [dep(X1, CC, (X\X)/X)|Deps1], Deps).
+fix_coord(_, Deps, Deps).
 
 %%      depdirs(+Style, +Cat, -Dirs)
 %
@@ -94,60 +110,99 @@ node2deps(Node, Style, Deps) :-
 %             and their arguments
 %           * =det= - like =mod=, but head-dependent relationship is also
 %             inverted for the arguments of =|np/n|= and =|np/(n/pp)|=
-%           * =alpino= like =det=, but conjunctions head both conjuncts
+%           * =bisk_nl=, =bisk_esdept=, =bisk_sv= - mimicking the dependencies
+%             created by Bisk and Hockenmaier (2013) for various treebanks,
+%             differing from =det= in how coordination is treated and by
+%             treating S and VP "modifiers" with different S features as
+%             modifiers.
+%           * =ud= - targetting Universal Dependencies; similar to =bisk_sv=.
 %
 %      Dirs is bound to a list of terms =normal= or =inverted=, corresponding
 %      to the arguments.
 
 depdirs(plain, Cat, Dirs) :-
-  depdirs(no, no, no, Cat, Dirs).
+  depdirs(no, x1_cc___cc_x2, no, no, Cat, Dirs).
 depdirs(mod, Cat, Dirs) :-
-  depdirs(yes, no, no, Cat, Dirs).
+  depdirs(feat_sensitive, x1_cc___cc_x2, no, no, Cat, Dirs).
 depdirs(det, Cat, Dirs) :-
-  depdirs(yes, no, yes, Cat, Dirs).
-depdirs(alpino, Cat, Dirs) :-
-  depdirs(yes, alpino, yes, Cat, Dirs).
+  depdirs(feat_sensitive, x1_cc___cc_x2, yes, no, Cat, Dirs).
+depdirs(bisk_nl, Cat, Dirs) :-
+  depdirs(feat_insensitive, cc_x1___cc_x2, yes, no, Cat, Dirs).
+depdirs(bisk_esdept, Cat, Dirs) :-
+  depdirs(feat_insensitive, x1_x2___x2_cc, yes, no, Cat, Dirs). % will be converted later by fix_coord/3
+depdirs(bisk_sv, Cat, Dirs) :-
+  depdirs(feat_insensitive, x1_x2___x2_cc, yes, no, Cat, Dirs).
+depdirs(ud, Cat, Dirs) :-
+  depdirs(feat_insensitive, x1_x2___x2_cc, yes, yes, Cat, Dirs).
 
-%%      depdirs(+Mod, +Coord, +Det, +Cat, -Dirs)
+%%      depdirs(+Mod, +Coord, +Det, +VPMod, +Prep, +Cat, -Dirs)
 %
 %       Computes the directions of functor-argument dependencies, depending on
 %       various features:
 %
-%           * Mod is one of =yes=, =no= - whether or not to invert
-%             modifier-argument dependencies
-%           * Coord is one of =no=, =alpino= - if =alpino=, conjunctions head
-%             both conjuncts.
+%           * Mod is one of =no=, =feat_sensitive=, =feat_insensitive= - =no=
+%             treats modifiers as heads of their arguments. =feat_sensitive=
+%             treats modifiers as dependents of their arguments.
+%             =feat_insensitive= likewise, but ignores features when
+%             determining whether or not a category is a modifier category.
+%           * Coord specifies the dependencies created by conjunction.
 %           * Det is one of =no=, =yes= - whether or not to treat determiners
 %             as dependents of their arguments.
+%           * Prep is one of =no=, =yes= - whether or not to treat prepostions
+%             as dependents of their arguments. TODO implement
 %
 %       The last two arguments are the input category (Cat) and the list of
 %       dependency directions (=normal= or =inverted=) for each argument.
-% Treat coordination Alpino-style if Coord=alpino
 
-depdirs(yes, alpino, Det, (X\X)/X, [noninv, noninv|Dirs]) :-
+% Treat coordination like cc_x1___cc_x2
+depdirs(Mod, cc_x1___cc_x2, Det, Prep, (X\X)/X, [normal, normal|Dirs]) :-
   !,
-  depdirs(yes, alpino, Det, X, Dirs).
-% Otherwise, treat modifier as dependent if Mod=yes
-depdirs(yes, Coord, Det, X/X, [inverted|Dirs]) :-
+  depdirs(Mod, cc_x1___cc_x2, Det, Prep, X, Dirs).
+% Treat coordination like x1_x2___x2_cc
+depdirs(Mod, x1_x2___x2_cc, Det, Prep, (X\X)/X, [inverted, inverted|Dirs]) :-
   !,
-  depdirs(yes, Coord, Det, X, Dirs).
-depdirs(yes, Coord, Det, X\X, [inverted|Dirs]) :-
+  depdirs(Mod, cc_x1___x2_cc, Det, Prep, X, Dirs).
+% Treat modifier as dependent
+depdirs(Mod, Coord, Det, Prep, X/Y, [inverted|Dirs]) :-
+  is_modifier_category(Mod, X/Y),
   !,
-  depdirs(yes, Coord, Det, X, Dirs).
-% Otherwise, treat determiners as dependent if Det=yes
-depdirs(_, _, yes, np/n, [inverted]) :-
+  depdirs(feat_sensitive, Coord, Det, Prep, X, Dirs).
+depdirs(Mod, Coord, Det, Prep, X\Y, [inverted|Dirs]) :-
+  is_modifier_category(Mod, X\Y),
+  !,
+  depdirs(feat_sensitive, Coord, Det, Prep, X, Dirs).
+% Treat prepositions as dependent if Prep=yes and Mod\=no
+depdirs(Mod, Coord, Det, yes, X/_, [inverted|Dirs]) :-
+  is_modifier_category(Mod, X),
+  !,
+  depdirs(Mod, Coord, Det, yes, X, Dirs).
+depdirs(Mod, Coord, Det, yes, X\_, [inverted|Dirs]) :-
+  is_modifier_category(Mod, X),
+  !,
+  depdirs(Mod, Coord, Det, yes, X, Dirs).
+% Treat determiners as dependent if Det=yes
+depdirs(_, _, yes, _, np/n, [inverted]) :-
   !.
-depdirs(_, _, yes, np/(n/pp), [inverted]) :-
+depdirs(_, _, yes, _, np/(n/pp), [inverted]) :-
   !.
 % Otherwise, treat argument as dependent
-depdirs(Mod, Coord, Det, Res/_, [normal|Dirs]) :-
+depdirs(Mod, Coord, Det, Prep, Res/_, [normal|Dirs]) :-
   !,
-  depdirs(Mod, Coord, Det, Res, Dirs).
-depdirs(Mod, Coord, Det, Res\_, [normal|Dirs]) :-
+  depdirs(Mod, Coord, Det, Prep, Res, Dirs).
+depdirs(Mod, Coord, Det, Prep, Res\_, [normal|Dirs]) :-
   !,
-  depdirs(Mod, Coord, Det, Res, Dirs).
+  depdirs(Mod, Coord, Det, Prep, Res, Dirs).
 % Base case: no argument
-depdirs(_, _, _, _, []).
+depdirs(_, _, _, _, _, []).
+
+is_modifier_category(feat_sensitive, X/X).
+is_modifier_category(feat_sensitive, X\X).
+is_modifier_category(feat_insensitive, A/B) :-
+  strip_features(A, X),
+  strip_features(B, X).
+is_modifier_category(feat_insensitive, A\B) :-
+  strip_features(A, X),
+  strip_features(B, X).
 
 %%      co_dirs_deps_tok_target(+Style, +Node, +CO, +Dirs, -Deps, -Tok, +Target0, -Target)
 %
